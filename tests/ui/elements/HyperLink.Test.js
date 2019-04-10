2cacfb9b442ed6b12a369173964706085b641e8f
DarrenPaulWright <darren@darrenpaulwright.com>
2019-04-10 07:55:35 -0700

    Fixing Hyperlink tests

--- tests/ui/elements/HyperLink.Test.js
+++ tests/ui/elements/HyperLink.Test.js
@@ -1,12 +1,12 @@
-import { HyperLink } from '../../../src';
+import { Hyperlink } from '../../../src';
 import TestUtil from '../../TestUtil';
 import ControlTests from '../ControlTests';
 
-const testUtil = new TestUtil(HyperLink);
-const controlTests = new ControlTests(HyperLink, testUtil, {
+const testUtil = new TestUtil(Hyperlink);
+const controlTests = new ControlTests(Hyperlink, testUtil, {
 	focusableElement: 'HyperLink'
 });
 
-describe('HyperLink', () => {
+describe('Hyperlink', () => {
 	controlTests.run(['width', 'height']);
 });
