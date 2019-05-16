a9dac369424e1bedd662c1ffca54bec52cc6b497
DarrenPaulWright <darren@darrenpaulwright.com>
2019-05-16 14:43:23 -0700

    Adding Description control

---
+++ src/ui/forms/Description.js
@@ -0,0 +1,98 @@
+import { AUTO, method } from 'type-enforcer';
+import dom from '../../utility/dom';
+import { LEFT, TEXT_ALIGN, WIDTH } from '../../utility/domConstants';
+import objectHelper from '../../utility/objectHelper';
+import controlTypes from '../controlTypes';
+import './Description.less';
+import FormControl from './FormControl';
+
+const COLUMNS_CLASS = 'columns';
+
+/**
+ * Display a description or other text. This control doesn't accept user input.
+ *
+ * @class Description
+ * @extends FormControl
+ * @constructor
+ *
+ * @arg {Object}  settings                    - Accepts all control and FormControl settings plus:
+ * @arg {String}  [settings.description]      - The default description to display
+ * @arg {Number}  [settings.textWidth]        - The width of the description text (not the control width)
+ */
+export default class Description extends FormControl {
+	constructor(settings = {}) {
+		settings.type = settings.type || controlTypes.DESCRIPTION;
+		super(settings);
+
+		const self = this;
+		self.addClass('description');
+
+		objectHelper.applySettings(self, settings);
+	}
+}
+
+Object.assign(Description.prototype, {
+	/**
+	 * @method value
+	 * @member module:Description
+	 * @instance
+	 * @arg {String} [value]
+	 * @returns {String|this}
+	 */
+	value: method.string({
+		set: function(value) {
+			dom.content(this, value);
+		}
+	}),
+
+	/**
+	 * @method textWidth
+	 * @member module:Description
+	 * @instance
+	 * @arg {String} [newTextWidth]
+	 * @returns {String|this}
+	 */
+	textWidth: method.string({
+		init: AUTO,
+		set: function(newValue) {
+			dom.css(this.contentContainer(), WIDTH, newValue);
+		}
+	}),
+
+	/**
+	 * @method align
+	 * @member module:Description
+	 * @instance
+	 * @arg {String} [newAlign] - Applys directly to the css property text-align.
+	 * @returns {String|this}
+	 */
+	align: method.string({
+		init: LEFT,
+		set: function(newValue) {
+			dom.css(this.contentContainer(), TEXT_ALIGN, newValue);
+		}
+	}),
+
+	/**
+	 * @method isColumns
+	 * @member module:Description
+	 * @instance
+	 *
+	 * @arg {boolean} [isColumns]
+	 *
+	 * @returns {boolean|this}
+	 */
+	isColumns: method.boolean({
+		set: function(isColumns) {
+			this.classes(COLUMNS_CLASS, isColumns);
+		}
+	}),
+
+	/**
+	 * @method isFocused
+	 * @member module:Description
+	 * @instance
+	 * @returns {Boolean} - Always returns false.
+	 */
+	isFocused: () => false
+});
---
+++ tests/ui/forms/Description.Test.js
@@ -0,0 +1,72 @@
+import { assert } from 'chai';
+import Description from '../../../src/ui/forms/Description';
+import dom from '../../../src/utility/dom';
+import { TEXT_ALIGN, WIDTH } from '../../../src/utility/domConstants';
+import { AUTO } from 'type-enforcer';
+import TestUtil from '../../TestUtil';
+import FormControlTests from './FormControlTests';
+
+const testUtil = new TestUtil(Description);
+const formControlTests = new FormControlTests(Description, testUtil, {
+	mainCssClass: 'description'
+});
+
+describe('Description', () => {
+
+	formControlTests.run();
+
+	describe('Value', () => {
+		testUtil.testMethod({
+			methodName: 'value',
+			defaultValue: '',
+			testValue: 'test String 2130478#$%&^#$%&',
+			secondTestValue: 'test String 2'
+		});
+	});
+
+	describe('TextWidth', () => {
+		testUtil.testMethod({
+			methodName: 'textWidth',
+			defaultValue: AUTO,
+			testValue: '100px',
+			secondTestValue: '200px'
+		});
+
+		it('should set the width of the content div to whatever is passed in to the textWidth option', () => {
+			window.control = new Description({
+				container: window.testContainer,
+				title: 'Test Title',
+				textWidth: '100px',
+				description: 'sample'
+			});
+
+			assert.equal(dom.css(document.querySelector('.form-control > div:first-of-type'), WIDTH), '100px');
+		});
+	});
+
+	describe('Align', () => {
+		testUtil.testMethod({
+			methodName: 'align',
+			defaultValue: 'left',
+			testValue: 'center',
+			secondTestValue: 'middle'
+		});
+
+		it('should set the text-align css property of the content div to whatever is passed in to the align option', () => {
+			window.control = new Description({
+				container: window.testContainer,
+				align: 'center'
+			});
+
+			assert.equal(dom.css(document.querySelector('div > div'), TEXT_ALIGN), 'center');
+		});
+	});
+
+	describe('Focused', () => {
+		it('should return false when the isFocused method is called', () => {
+			window.control = new Description();
+
+			assert.equal(window.control.isFocused(), false);
+		});
+	});
+});
