34948c9f58b35bf561c6594b7a5e12ec4769e431
DarrenPaulWright <darren@darrenpaulwright.com>
2019-09-06 09:39:22 -0700

    Removing class Element

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 export { default as theme } from './ui/themes/theme';
 
 export { default as controlTypes } from './ui/controlTypes';
-export { default as Element } from './ui/Element';
 export { default as Control, ELEMENT_PROP } from './ui/Control';
 export { default as ControlManager } from './ui/ControlManager';
 export { default as ControlRecycler } from './ui/ControlRecycler';
--- src/ui/media/Source.js
+++ src/ui/media/Source.js
@@ -1,9 +1,9 @@
 import { applySettings } from 'type-enforcer';
 import dom from '../../utility/dom';
+import Control from '../Control';
 import controlTypes from '../controlTypes';
-import Element from '../Element';
 
-export default class Source extends Element {
+export default class Source extends Control {
 	constructor(settings = {}) {
 		settings.type = settings.type || controlTypes.SOURCE;
 		settings.element = dom.buildNew(null, 'source');
